5d6ea0b58a50611a895e9395f5f3b77e9f314ef1
minsang kim <mskim3747@gmail.com>
2024-03-28 17:54:11 +0900

    feat : Zombie Controller 구현 중

--- Assets/Scripts/Contorller/Zombie Controller.cs
+++ Assets/Scripts/Contorller/Zombie Controller.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieController : MonoBehaviour
 {
-    
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        
+    }
 }
