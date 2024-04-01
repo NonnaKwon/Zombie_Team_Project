7edd5649e858b8e050becd4fb7d9a4045a53f758
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2024-04-01 16:46:12 +0900

    feat : 타이머 구현

--- Assets/Scripts/UI/Scene/UI_GameScene.cs
+++ Assets/Scripts/UI/Scene/UI_GameScene.cs
@@ -1,24 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UI_GameScene : InGameUI
 {
-    enum GameObject
+    private float _passTime = 0;
+    enum GameObjects
     {
         PlayerHP,
         HungerSlider,
         ThirstSlider,
         FatigueSlider,
         StaminaSlider,
-        Hour,
+        Sec,
         Min,
         Mission
     }
 
     private void Start()
     {
-        
+
+    }
+
+    private void Update()
+    {
+        _passTime += Time.deltaTime;
+        GetUI<TMP_Text>(GameObjects.Min.ToString()).text = ((int)_passTime / 60).ToString("D2");
+        GetUI<TMP_Text>(GameObjects.Sec.ToString()).text = ((int)_passTime % 60).ToString("D2");
     }
 
 }
