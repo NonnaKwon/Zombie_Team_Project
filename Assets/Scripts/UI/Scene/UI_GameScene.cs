8a0e74c7c28a4674125b970fa4bc68d25c6de3b8
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2024-04-01 11:42:50 +0900

    feat : 인게임 UI 만들기(프로토타입)

---
+++ Assets/Scripts/UI/Scene/UI_GameScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_GameScene : InGameUI
+{
+    enum GameObject
+    {
+        PlayerHP,
+        HungerSlider,
+        ThirstSlider,
+        FatigueSlider,
+        StaminaSlider,
+        Hour,
+        Min,
+        Mission
+    }
+
+    private void Start()
+    {
+        
+    }
+
+}
