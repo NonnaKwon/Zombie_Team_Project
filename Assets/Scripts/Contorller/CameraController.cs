6c8283c159145f8eede5a276e50b7768bb6da1fa
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2024-04-08 16:23:50 +0900

    Revert "Revert "Merge branch 'develop' into Invectory""
    This reverts commit f2ecf65d8b80b48a5ce05b0079c72778c53a9657.

--- Assets/Scripts/Contorller/CameraController.cs
+++ Assets/Scripts/Contorller/CameraController.cs
@@ -13,7 +13,7 @@ public class CameraController : MonoBehaviour
         _follow = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         _distance = transform.position - _follow.position;
     }
-    private void LateUpdate()
+    private void Update()
     {
         transform.position = new Vector3(_follow.position.x, 0, _follow.position.z) + _distance;
     }
